5077f51c229efe13eb1459ba6e57bebd028236d8
Robbe Boon <robbe.boon@student.hogent.be>
2020-09-24 11:23:36 +0200

    Initial commit

---
+++ Models/Domain/BankAccount.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Banking.Models.Domain
+{
+    public class BankAccount
+    {
+        /* private readonly string _accountNumber;
+          private decimal _balance;
+          //Constante aanmaken voor decimal, "M" staat voor money, dus hij blijft decimal
+          //public const decimal WithdrawCost = 0.25M;
+          public decimal Balance
+          {
+              get { return _balance; }
+              private set { _balance = value; }
+          }*/
+        #region Properties
+        public decimal Balance { get; set; } = 0M;
+        public string AccountNumber { get; }
+        #endregion
+        #region Contructor
+        public BankAccount(string accountNumber)
+        {
+            throw new NotImplementedException();
+        }
+        #endregion
+        #region Methods
+        public void Deposit(decimal amount)
+        {
+            Balance += amount;
+
+        }
+        public void Withdraw(decimal amount)
+        {
+            Balance -= amount;
+        }
+        #endregion
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,12 +1,23 @@
-﻿using System;
+﻿using Banking.Models.Domain;
+using System;
 
 namespace Banking
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            BankAccount account = new BankAccount("133-15615651-12");
+            account.Deposit(200);
+            WriteToConsole(account);
+            account.Withdraw(100);
+            WriteToConsole(account);
+        }
+
+        public static void WriteToConsole(BankAccount account)
+        {
+            Console.WriteLine($"Balance: {account.Balance}");
         }
     }
 }
